7d6707eec4b81748bff798e780c5d7334c46a383
Sumith Harshan <sumith@surge.global>
2022-07-20 15:08:11 +0530

    crypto cal added

---
+++ sgem-crypto-calculator/assets/js/e-crypto-ira-min.js
@@ -0,0 +1,19 @@
+ //window.onload = function() {
+ if (!window.jQuery) {  
+   var sgecimj = document.createElement('script');
+   sgecimj.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js');
+   document.head.appendChild(sgecimj);
+ } 
+//}
+var sgem_ci_head1  = document.getElementsByTagName('head')[0];
+let sgemcijs3 = document.createElement('script');
+   sgemcijs3.setAttribute('src','https://cdn.jsdelivr.net/npm/chart.js');
+  sgem_ci_head1.appendChild(sgemcijs3);
+   
+ setTimeout(runCal, 500);
+  
+function runCal(){ 
+   let sgemcijs = document.createElement('script');
+   sgemcijs.setAttribute('src','https://messy-saxophone.flywheelsites.com/wp-content/calculators-html/crypto-demo/e-crypto-ira-all.js?v=2');
+   document.body.appendChild(sgemcijs);
+} 
