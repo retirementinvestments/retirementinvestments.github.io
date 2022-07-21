16c631cc95a20b526cd1bf71affb65d4a6de94ed
Sumith Harshan <sumith@surge.global>
2022-07-21 22:06:22 +0530

    logo moved to bottom, disclaimer

--- sgem-investment-cal/assets/js/e-cal-scripts-min.js
+++ sgem-investment-cal/assets/js/e-cal-scripts-min.js
@@ -5,6 +5,10 @@
    document.head.appendChild(sgemj);
  } 
 //}
+var sgem_head  = document.getElementsByTagName('head')[0];var sgem_xs  = document.getElementsByTagName('head')[0];var sgem_style  = document.createElement('link');sgem_style.id   = 'sgem-re-cal-styles';
+sgem_style.rel  = 'stylesheet';sgem_style.type = 'text/css';sgem_style.href = 'https://retirementinvestments.github.io/sgem-investment-cal/assets/css/styles.min.css';
+sgem_style.media = 'all';sgem_head.appendChild(sgem_style);
+
  setTimeout(runCal, 1000);
 
 function runCal(){
