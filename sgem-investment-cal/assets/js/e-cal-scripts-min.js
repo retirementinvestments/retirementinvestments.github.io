7964c587187c416da069966879aac3b349592c3d
Sumith Harshan <sumith@surge.global>
2022-07-22 16:45:32 +0530

    obfuscated

--- sgem-investment-cal/assets/js/e-cal-scripts-min.js
+++ sgem-investment-cal/assets/js/e-cal-scripts-min.js
@@ -1,18 +1,1 @@
- //window.onload = function() {
- if (!window.jQuery) {  
-   var sgemj = document.createElement('script');
-   sgemj.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js');
-   document.head.appendChild(sgemj);
- } 
-//}
-var sgem_head  = document.getElementsByTagName('head')[0];var sgem_xs  = document.getElementsByTagName('head')[0];var sgem_style  = document.createElement('link');sgem_style.id   = 'sgem-re-cal-styles';
-sgem_style.rel  = 'stylesheet';sgem_style.type = 'text/css';sgem_style.href = 'https://retirementinvestments.github.io/sgem-investment-cal/assets/css/styles.min.css';
-sgem_style.media = 'all';sgem_head.appendChild(sgem_style);
-
- setTimeout(runCal, 1000);
-
-function runCal(){
-   let sgemjs = document.createElement('script');
-   sgemjs.setAttribute('src','https://retirementinvestments.github.io/sgem-investment-cal/assets/js/e-cal-scripts-min-all.js');
-   document.body.appendChild(sgemjs);
-}
+if(!window.jQuery){var a=document.createElement("script");a.setAttribute("src","https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"),document.head.appendChild(a)}var sgem_head=document.getElementsByTagName("head")[0],sgem_xs=document.getElementsByTagName("head")[0],sgem_style=document.createElement("link");function runCal(){let a=document.createElement("script");a.setAttribute("src","https://retirementinvestments.github.io/sgem-investment-cal/assets/js/e-cal-scripts-min-all.js"),document.body.appendChild(a)}sgem_style.id="sgem-re-cal-styles",sgem_style.rel="stylesheet",sgem_style.type="text/css",sgem_style.href="https://retirementinvestments.github.io/sgem-investment-cal/assets/css/styles.min.css",sgem_style.media="all",sgem_head.appendChild(sgem_style),setTimeout(runCal,1e3)
