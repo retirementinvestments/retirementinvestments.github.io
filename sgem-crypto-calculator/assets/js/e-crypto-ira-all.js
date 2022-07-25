fdc7d096923fafad968d935eece3f9a47c0626f9
Sumith Harshan <sumith@surge.global>
2022-07-25 14:33:38 +0530

    footer text linked

--- sgem-crypto-calculator/assets/js/e-crypto-ira-all.js
+++ sgem-crypto-calculator/assets/js/e-crypto-ira-all.js
@@ -49,7 +49,7 @@ sgem_crypto_main_content += '</div>';
 sgem_crypto_main_content += '</div>';
 sgem_crypto_main_content += '</div>';
 
-sgem_crypto_main_content += '<div class="sgem-crypto-ira-logo-center"><a class="sgem-crypto-ira-url-text" href="https://retirementinvestments.com/bitcoinira" target="_blank" rel="noopener">Crypto IRA Calculator by</a> Retirement investments</div>';
+sgem_crypto_main_content += '<div class="sgem-crypto-ira-logo-center"><a class="sgem-crypto-ira-url-text" href="https://retirementinvestments.com/crypto/crypto-ira-calculator/" target="_blank" rel="noopener">Crypto IRA Calculator by</a> Retirement investments</div>';
 
 sgem_crypto_main_content += '<div class="sgem-crypto-ira-copy-option-panel">';
 sgem_crypto_main_content += '<h3>Do you want to add this calculator into your website?</h3>';
