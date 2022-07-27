8ea90fd7f5b837e9ab96fc33cde7601091b81fd1
Sumith Harshan <sumith@surge.global>
2022-07-27 22:02:52 +0530

    sgem_pmc_ConvertToInternationalCurrencySystem added

--- sgem-precious-metal-cal/assets/js/e-pmc-scripts-min-all.js
+++ sgem-precious-metal-cal/assets/js/e-pmc-scripts-min-all.js
@@ -268,7 +268,29 @@ function isNumber(evt) {
           }
           return true;
 }
-    
+ 
+
+ 
+function sgem_pmc_ConvertToInternationalCurrencySystem(labelValue) {
+
+    // Nine Zeroes for Billions
+    return Math.abs(Number(labelValue)) >= 1.0e+9
+
+    ? (Math.abs(Number(labelValue)) / 1.0e+9).toFixed(2) + "B"
+    // Six Zeroes for Millions 
+    : Math.abs(Number(labelValue)) >= 1.0e+6
+
+    ? (Math.abs(Number(labelValue)) / 1.0e+6).toFixed(2) + "M"
+    // Three Zeroes for Thousands
+    : Math.abs(Number(labelValue)) >= 1.0e+3
+
+    ? (Math.abs(Number(labelValue)) / 1.0e+3).toFixed(2) + "K"
+
+    : Math.abs(Number(labelValue));
+
+}
+
+ 
 function sgem_pmc_ConvertToInternationalCurrencySystemLabel(labelValue) {
 
     // Nine Zeroes for Billions
