7fa6b71dbedcedf818046c53f9507cc51b5503c7
Sumith Harshan <sumith@surge.global>
2022-08-04 19:13:51 +0530

    Update e-cal-scripts-min-all.js

--- sgem-investment-cal/assets/js/e-cal-scripts-min-all.js
+++ sgem-investment-cal/assets/js/e-cal-scripts-min-all.js
@@ -686,30 +686,30 @@ function sgem_r_copyText(ev){
  
  
  	
-	$(window).on('resize', function() {
+	jQuery(window).on('resize', function() {
 
-		if ($('.sgem-retirement-cal').width() < 700) {
-	      $('.sgem-cal-wrapper').addClass('sgem-reti-cal-width-add');
-	    	$('.sgem-reti-cal-input-width').addClass('sgem-reti-cal-max-width-style-add');
-	    	$('.sgem-cal-left').addClass('sgem-cal-left-add-class').addClass('sgem-reti-cal-scroll-height');
-	    	$('.sgem-reti-cal-input-width').addClass('sgem-reti-cal-input-width-mobile');
+		if (jQuery('.sgem-retirement-cal').width() < 700) {
+	      jQuery('.sgem-cal-wrapper').addClass('sgem-reti-cal-width-add');
+	    	jQuery('.sgem-reti-cal-input-width').addClass('sgem-reti-cal-max-width-style-add');
+	    	jQuery('.sgem-cal-left').addClass('sgem-cal-left-add-class').addClass('sgem-reti-cal-scroll-height');
+	    	jQuery('.sgem-reti-cal-input-width').addClass('sgem-reti-cal-input-width-mobile');
 	  }
 		else {
-			$('.sgem-cal-wrapper').removeClass('sgem-reti-cal-width-add');
-	    $('.sgem-reti-cal-input-width').removeClass('sgem-reti-cal-max-width-style-add');
-	    $('.sgem-cal-left').removeClass('sgem-cal-left-add-class').removeClass('sgem-reti-cal-scroll-height');
-	    $('.sgem-reti-cal-input-width').removeClass('sgem-reti-cal-input-width-mobile');
+		jQuery('.sgem-cal-wrapper').removeClass('sgem-reti-cal-width-add');
+	    jQuery('.sgem-reti-cal-input-width').removeClass('sgem-reti-cal-max-width-style-add');
+	    jQuery('.sgem-cal-left').removeClass('sgem-cal-left-add-class').removeClass('sgem-reti-cal-scroll-height');
+	    jQuery('.sgem-reti-cal-input-width').removeClass('sgem-reti-cal-input-width-mobile');
 		}
 	}).trigger('resize');
 
 	$(window).on('resize', function() {
 
-		if ($('.sgem-retirement-cal').width() < 960) {
-	    	$('.sgem-cal-left').addClass('sgem-cal-left-add-class');
-	    	$('.sgem-cal-right').addClass('sgem-cal-right-add-class');
-	  }
+		if (jQuery('.sgem-retirement-cal').width() < 960) {
+	    	jQuery('.sgem-cal-left').addClass('sgem-cal-left-add-class');
+	    	jQuery('.sgem-cal-right').addClass('sgem-cal-right-add-class');
+	    }
 		else {
-	    $('.sgem-cal-left').removeClass('sgem-cal-left-add-class');
-	    $('.sgem-cal-right').removeClass('sgem-cal-right-add-class');
+	    jQuery('.sgem-cal-left').removeClass('sgem-cal-left-add-class');
+	    jQuery('.sgem-cal-right').removeClass('sgem-cal-right-add-class');
 		}
 	}).trigger('resize');
