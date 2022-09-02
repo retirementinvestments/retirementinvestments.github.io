00e3a01f926ee4b380181499ad26bd06127ca686
retirementinvestments <websites@surge.global>
2022-09-02 15:10:25 +0530

    update decimal point value

--- sgem-investment-cal/assets/js/e-cal-scripts-min-all.js
+++ sgem-investment-cal/assets/js/e-cal-scripts-min-all.js
@@ -431,10 +431,7 @@ if( location.hostname == "messy-saxophone.flywheelsites.com") {
 
 // comma separate values
 function numberWithCommas(num) {
-  // return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-
-  var Mynumber = parseFloat(num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')).toFixed(4);
-  return Mynumber;
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 // end
 
