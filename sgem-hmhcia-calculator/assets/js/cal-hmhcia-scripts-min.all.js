2934cd6d29a207fcf4f276445d95a6ea9921587d
retirementinvestments <websites@surge.global>
2022-10-04 16:45:27 +0530

    update css

--- sgem-hmhcia-calculator/assets/js/cal-hmhcia-scripts-min.all.js
+++ sgem-hmhcia-calculator/assets/js/cal-hmhcia-scripts-min.all.js
@@ -1179,8 +1179,8 @@ function sgemGenarateSlider2(calby) {
     var width = sgemimghouse.width;
     var height = sgemimghouse.height;
     ranger.onchange = function() {
-        sgemimgpig.width = width / (ranger.value / 2000);
-        sgemimgpig.height = height / (ranger.value / 2000);
+        sgemimgpig.width = width / (ranger.value / 700);
+        sgemimgpig.height = height / (ranger.value / 700);
 
         sgemimghouse.width = width * (ranger.value / 4000);
         sgemimghouse.height = height * (ranger.value / 4000);
