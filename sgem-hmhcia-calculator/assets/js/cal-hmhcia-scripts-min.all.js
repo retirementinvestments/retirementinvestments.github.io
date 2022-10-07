e2e3a9bc9247dd4a42d37d2b32dc066bf09259d9
retirementinvestments <websites@surge.global>
2022-10-07 16:23:23 +0530

    update js

--- sgem-hmhcia-calculator/assets/js/cal-hmhcia-scripts-min.all.js
+++ sgem-hmhcia-calculator/assets/js/cal-hmhcia-scripts-min.all.js
@@ -1252,10 +1252,11 @@ function sgemGenarateSlider(calby) {
 
         var sliderOnePig = ( parseFloat(25) / parseFloat(rangerOne.value) ) * parseFloat(22); 
         var sliderOnePig1 = (sliderOnePig / parseFloat(10)) * parseFloat(25);
+
         if (sliderOnePig1 > 200 ) {
             rangerValueSlide = (parseFloat(sliderOnePig1) - parseFloat(sliderOnePig1)) + parseFloat(80);
             $('#sgem-hmhcia-image-pig2').css('width',rangerValueSlide + 'px');
-        } else (sliderOnePig1 < 200) {
+        } else {
             $('#sgem-hmhcia-image-pig2').css('width',sliderOnePig1 + 'px');
         }
         
