106a2868d4e972c0714199303f77a7ccdea8c43b
retirementinvestments <websites@surge.global>
2022-11-10 09:55:52 +0530

    update copy code js

--- sgem-mrc-calculator/assets/js/cal-mrc-scripts-min.all.js
+++ sgem-mrc-calculator/assets/js/cal-mrc-scripts-min.all.js
@@ -1073,12 +1073,12 @@ tippy('[data-tippy-content]', {
 
 
 // copy script section
-localStorage.setItem('sgem-mrc-copy-code', '<div id="sgem-mrc-copy-code"></div><script>window.onload = function() {var sgemmrccal = document.createElement("script");sgemmrccal.type = "text/javascript";sgemmrccal.src = "https://retirementinvestments.github.io/sgem-mrc-calculator/assets/js/cal-mrc-scripts.min.js";document.body.appendChild(sgemmrccal);} </script>'); 
+localStorage.setItem('sgem-mrc-cal', '<div id="sgem-mrc-cal"></div><script>window.onload = function() {var sgemmrccal = document.createElement("script");sgemmrccal.type = "text/javascript";sgemmrccal.src = "https://retirementinvestments.github.io/sgem-mrc-calculator/assets/js/cal-mrc-scripts.min.js";document.body.appendChild(sgemmrccal);} </script>'); 
 
 function sgem_mrc_copyText(ev){
   //console.log("hi");
   let div = document.getElementById('div');
-  let text = localStorage.getItem('sgem-mrc-copy-code');
+  let text = localStorage.getItem('sgem-mrc-cal');
   let textArea  = document.createElement('textarea');
   textArea.width  = "1px"; 
   textArea.height = "1px";
