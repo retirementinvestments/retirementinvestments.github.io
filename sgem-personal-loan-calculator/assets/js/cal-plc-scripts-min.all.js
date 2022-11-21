e57bffbf9d2f8d5199dfd3d077b6e594108b78f4
retirementinvestments <websites@surge.global>
2022-11-21 16:37:29 +0530

    update button and footer urls

--- sgem-personal-loan-calculator/assets/js/cal-plc-scripts-min.all.js
+++ sgem-personal-loan-calculator/assets/js/cal-plc-scripts-min.all.js
@@ -129,7 +129,7 @@ sgem_plc_main_contents += '</div></details></div></div></div></div>';
 
 
 if( (location.hostname != "calculatorstg.wpengine.com") || (location.hostname != "retirementinvestments.com") ){
-    sgem_plc_main_contents += '<div class="sgem-plc-logo-center"><a class="sgem-plc-logo-image" href="https://retirementinvestments.com/real-estate/refinance-calculator/" target="_blank" rel="noopener">';
+    sgem_plc_main_contents += '<div class="sgem-plc-logo-center"><a class="sgem-plc-logo-image" href="https://retirementinvestments.com/loans/personal-loan-calculator/" target="_blank" rel="noopener">';
     sgem_plc_main_contents += 'Personal Loan Calculator</a>&nbsp; by Retirement Investments</div>';
 }
 
