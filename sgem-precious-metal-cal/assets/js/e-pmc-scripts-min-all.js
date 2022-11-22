9241959a8a1d9bb95d9b2ec87ce1321fefb135c2
retirementinvestments <websites@surge.global>
2022-11-22 11:05:40 +0530

    update tooltip color

--- sgem-precious-metal-cal/assets/js/e-pmc-scripts-min-all.js
+++ sgem-precious-metal-cal/assets/js/e-pmc-scripts-min-all.js
@@ -234,9 +234,9 @@ var config = {
           callbacks: {
             labelTextColor: function(context){
                 // return myChart.data.datasets.backgroundColor;
-                // return '#757575';
-                console.log(context);
-                return context.dataset.backgroundColor[context.dataIndex];
+                return '#757575';
+                // console.log(context);
+                // return context.dataset.backgroundColor[context.dataIndex];
             }, 
             label: function(context) {
               return context.dataset.labels + ': ' + sgem_pmc_ConvertToInternationalCurrencySystemRound(context.dataset.data[context.dataIndex])
