974320b7585d14e85f51e437c9892539be5731ea
retirementinvestments <websites@surge.global>
2022-11-22 13:19:39 +0530

    change chart js url

--- sgem-401k-calculator/assets/js/cal-401k-cal-scripts.min.all.js
+++ sgem-401k-calculator/assets/js/cal-401k-cal-scripts.min.all.js
@@ -1374,6 +1374,6 @@ function sgem_401k_copyText(ev){
 
 window.addEventListener('load', function() {     
     let sgemjsxc = document.createElement('script');
-       sgemjsxc.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.3.0/chart.min.js');
+       sgemjsxc.setAttribute('src','https://retirementinvestments.github.io/sgem-401k-calculator/assets/js/chartjs.min.js');
        document.body.appendChild(sgemjsxc);
 });
