c3c725630a34ddf3307f443317779c40c4215544
retirementinvestments <websites@surge.global>
2022-11-23 12:06:46 +0530

    update hostname conditions

--- sgem-401k-calculator/assets/js/cal-401k-cal-scripts.min.all.js
+++ sgem-401k-calculator/assets/js/cal-401k-cal-scripts.min.all.js
@@ -183,11 +183,11 @@ sgem_401k_main_contents += '</div>';
 sgem_401k_main_contents += '</div>';
 sgem_401k_main_contents += '</div>';
 
-if( (location.hostname != "calculatorstg.wpengine.com") || (location.hostname != "retirementinvestments.com") ){
+if( (location.hostname != "calculatorstg.wpengine.com") || (location.hostname != "retirementinvestments.com") || (location.hostname != "staging.retirementinvestments.com") ){
     sgem_401k_main_contents += '<div class="sgem-401k-logo-center"><a class="sgem-401k-url-text" href="https://retirementinvestments.com/retirement/401k-calculator" target="_blank" rel="noopener">401(k) Calculator  </a>&nbsp;by Retirement investments</div>';
 }
 
-if(location.hostname == "calculatorstg.wpengine.com" || location.hostname == "retirementinvestments.com"){
+if(location.hostname == "calculatorstg.wpengine.com" || location.hostname == "retirementinvestments.com" || location.hostname == "staging.retirementinvestments.com"){
     sgem_401k_main_contents += '<div class="sgem-401k-copy-option-panel">';
     sgem_401k_main_contents += '<h3>Do you want to add this calculator into your website?</h3>';
     sgem_401k_main_contents += '<div class="sgem-401k-copy-code-wrap"><button class="sgem-401k-copy-code" id="sgem-401k-copy-code" onclick="sgem_401k_copyText(event)">Get Calculator</button></div>';
@@ -394,6 +394,8 @@ jQuery(document).ready(function($){
     if( location.hostname == "calculatorstg.wpengine.com") {
         $('.sgem-401k-logo-center').hide(); 
     } else if(location.hostname == "retirementinvestments.com"){
+        $('.sgem-401k-logo-center').hide();  
+    } else if(location.hostname == "staging.retirementinvestments.com"){
         $('.sgem-401k-logo-center').hide();  
     } else {
         $('.sgem-401k-logo-center').show();  
