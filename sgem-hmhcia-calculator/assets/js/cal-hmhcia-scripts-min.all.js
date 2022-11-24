e6220e2eacc767501344277acbecd46c242dac1e
retirementinvestments <websites@surge.global>
2022-11-24 11:19:58 +0530

    chart js cdn url replace to local (v3.8.0)

--- sgem-hmhcia-calculator/assets/js/cal-hmhcia-scripts-min.all.js
+++ sgem-hmhcia-calculator/assets/js/cal-hmhcia-scripts-min.all.js
@@ -1541,6 +1541,7 @@ function sgem_hmhcia_copyText(ev){
   alert('Code snippet copied to clipboard!');
 }
 
+// chart js cdn url replace to local (v3.8.0)
 window.addEventListener('load', function() {     
     let sgemjsxc = document.createElement('script');
        sgemjsxc.setAttribute('src','https://retirementinvestments.github.io/sgem-hmhcia-calculator/assets/js/plugins/chartjs.min.js');
