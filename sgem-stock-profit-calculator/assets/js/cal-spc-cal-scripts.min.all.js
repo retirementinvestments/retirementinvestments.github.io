135a669f801c2b2bdc4dba49120529677ec90905
retirementinvestments <websites@surge.global>
2023-01-31 11:48:41 +0530

    CSS blog post adjustment

--- sgem-stock-profit-calculator/assets/js/cal-spc-cal-scripts.min.all.js
+++ sgem-stock-profit-calculator/assets/js/cal-spc-cal-scripts.min.all.js
@@ -376,8 +376,13 @@ $(document).ready(function() {
 
     if ($('.sgem-spc-cal-main-id').width() < 650) {
         $('.sgem-spc-cal-wrapper').addClass('sgem-spc-wrapper-add-mobile');
+		$('.sgem-spc-content').addClass('sgem-switch-flex');
+		$('.sgem-spc-cal-wrapper').addClass('sgem-switch-flex');
+		
     } else {
         $('.sgem-spc-cal-wrapper').removeClass('sgem-spc-wrapper-add-mobile');
+		$('.sgem-spc-content').removeClass('sgem-switch-flex');
+		$('.sgem-spc-cal-wrapper').removeClass('sgem-switch-flex');
     }
 
     $(window).on('resize', function() {
@@ -393,8 +398,12 @@ $(document).ready(function() {
     $(window).on('resize', function() {
         if ($('.sgem-spc-cal-main-id').width() < 650) {
             $('.sgem-spc-cal-wrapper').addClass('sgem-spc-wrapper-add-mobile');
+			$('.sgem-spc-content').addClass('sgem-switch-flex');
+			$('.sgem-spc-cal-wrapper').addClass('sgem-switch-flex');
         } else {
             $('.sgem-spc-cal-wrapper').removeClass('sgem-spc-wrapper-add-mobile');
+			$('.sgem-spc-content').removeClass('sgem-switch-flex');
+			$('.sgem-spc-cal-wrapper').removeClass('sgem-switch-flex');
         }
     }).trigger('resize');
 
